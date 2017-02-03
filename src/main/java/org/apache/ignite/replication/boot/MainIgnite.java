fce8af68be9cec935ae33be256b04b0030049b52
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2017-02-03 13:50:10 +0400

    ignite starter

---
+++ src/main/java/org/apache/ignite/replication/boot/MainIgnite.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.replication.boot;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+public class MainIgnite {
+    public static void main(String[] args) {
+        Ignite ignite = Ignition.start("ignite-config.xml");
+
+        //todo add here your code
+    }
+}
